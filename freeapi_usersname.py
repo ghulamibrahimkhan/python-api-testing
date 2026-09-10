2528918ac25ecc0d384d60687e962f94c87667c8
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2026-09-10 20:25:34 +0500

    crate main function

--- freeapi_usersname.py
+++ freeapi_usersname.py
@@ -1,15 +1,25 @@
 import requests
 
-def get_random_usernames():
-    url = "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10"
-    response = requests(url)
+def fetch_random_user_api():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
     data = response.json()
 
-    if data["sucess"] and "data" in data:
+    if data["success"] and "data" in data:
         user_data = data["data"]
-        user_name = user_data["login"]["user_name"]
+        username = user_data["login"]["username"]
         country = user_data["location"]["country"]
-        return user_data, country
+        return username , country
     else:
         raise Exception("Failed To Fetch User Data")
 
+def main():
+    try:
+        username, country = fetch_random_user_api()
+        print(username)
+        print(country)
+    except Exception as request_failed:
+        print(print(str(request_failed)))
+
+if __name__ == "__main__":
+    main()
